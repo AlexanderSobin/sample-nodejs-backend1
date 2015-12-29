b0ae480c50e55698f46b4f65331acee4857bbba1
Anton Akentiev <anthony.akentiev@gmail.com>
2015-12-29 19:32:11 +0300

    Adding Facebook login support

--- server.js
+++ server.js
@@ -134,6 +134,7 @@ app.get('/prepShutdown', function(req, res) {
 
 // This is main APIs file
 eval(fs.readFileSync('requests/users.js')+'');
+eval(fs.readFileSync('requests/facebook.js')+'');
 eval(fs.readFileSync('requests/static_pages.js')+'');
 
 function initDb(dbInit){
